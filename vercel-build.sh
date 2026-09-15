c5d7f5cc80e5e29605cce5ccee173f8c3cd8edc7
Mario Moncada <memoncada05@gmail.com>
2026-09-15 11:49:17 -0600

    fix: mover build command a script separado por limite de caracteres de vercel

---
+++ vercel-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "SUPABASE_URL=$SUPABASE_URL" > .env
+echo "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" >> .env
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 _flutter
+export PATH="$PATH:$(pwd)/_flutter/bin"
+
+flutter pub get
+flutter build web --release
